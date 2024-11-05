1715bb0abe15592f5c6a6ec9ed56b06109397b42
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-11-05 20:18:47 +0000

    task 1

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
+"""A Basic Flask app.
 """
-basics of flask
-"""
-
 from flask import Flask, render_template
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
-def home() -> str:
+def get_index() -> str:
+    """The home/index page.
     """
-    home/template pagr
-    """
-    return render_template('index.html')
+    return render_template('0-index.html')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
